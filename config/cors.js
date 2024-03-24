cf3be393352355619e61145b853e5b40e947e47a
Geeleed <surasak.kaewpho@gmail.com>
2024-03-25 00:07:37 +0700

    rev cors

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,10 @@
 const cors = require("cors");
 module.exports = cors({
   credentials: true,
-  origin: [process.env.FRONTEND_ORIGIN, "http://localhost:3000"],
+  origin: [
+    process.env.FRONTEND_ORIGIN,
+    "http://localhost:3000",
+    "https://geeleed-hotel.vercel.app",
+  ],
 });
 // router.use(require("./cors"));

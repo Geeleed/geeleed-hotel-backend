cf7e03fa83900ba3c8a071d8da36fd55edf7aa29
Geeleed <surasak.kaewpho@gmail.com>
2024-03-29 16:27:14 +0700

    debug delete roomCard

--- routers/Room/delete.js
+++ routers/Room/delete.js
@@ -5,10 +5,10 @@ router.use(require("../../config/cors"));
 const Room = require("./../../models/Room")();
 router.delete("/deleteRoomCard/:id", async (req, res) => {
   try {
-    let roomCardId = new mongoose.Types.ObjectId(req.params.id);
-    if (!roomCardId || !mongoose.Types.ObjectId.isValid(roomCardId))
-      throw "invalid id";
-    const result = await Room.findByIdAndDelete(roomCardId);
+    // let roomCardId = new mongoose.Types.ObjectId(req.params.id);
+    // if (!roomCardId || !mongoose.Types.ObjectId.isValid(roomCardId))
+    //   throw "invalid id";
+    await Room.findByIdAndDelete(req.params.id);
     res.json({ message: "deleted" });
   } catch (e) {
     console.error(e);
